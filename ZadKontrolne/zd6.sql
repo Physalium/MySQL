d1c198980a6c1ce9525224428a7a90978fdf6a4b
Karol <physalium2@gmail.com>
2020-04-23 22:13:25 +0200

    czesc zrobiona

--- ZadKontrolne/zd6.sql
+++ ZadKontrolne/zd6.sql
@@ -17,10 +17,50 @@ limit 1;
 
 -- 3
 
-select Continent, city.Name
+select Continent, MAX(city.Population)
 from country,
      city
-where city.CountryCode=country.Code
-group by Continent, city.Name
-order by city.Population desc
-limit 12;
+where CountryCode = Code
+group by 1;
+
+-- 4
+
+select Continent, count(*) "Ilosc miast"
+from country,
+     city c
+where c.CountryCode = Code
+group by Continent;
+
+-- 5
+select city.Name "Miasto", country.Name "Panstwo"
+from city,
+     country
+where city.Name = country.Name
+  and city.CountryCode = Code;
+
+-- 6
+select Name, language
+from country,
+     countrylanguage
+where CountryCode = Code
+  and (Language = Name or Language = null);
+
+-- 7
+select m.Name, j.Language
+from country k,
+     city m,
+     countrylanguage j
+where m.CountryCode = k.Code
+  and j.Language = "Polish"
+  and k.Code = j.CountryCode;
+
+-- 8
+
+select j.Language, count(j.Language) ile
+from country k,
+     countrylanguage j
+where j.CountryCode = k.Code
+group by 1
+having ile > 5
+order by 2 desc;
+
